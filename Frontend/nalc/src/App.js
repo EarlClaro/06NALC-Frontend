06a6726859e3e8cfa2d920fb7646ab9f9e3f40a0
Andre Lennard S. Uy <andrelennard.uy@cit.edu>
2024-02-03 19:49:04 +0800

    modify app.js

--- Frontend/nalc/src/App.js
+++ Frontend/nalc/src/App.js
@@ -10,8 +10,9 @@ import 'ldrs/ring'
 function App() {
   return (
     <>
-    <marquee>Developed by Team Wangan &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
-       Members: Barcenilla Adrian Jay , Garcia Victor , Miones John William , Taboada Timothy , Uy Andre Lennard</marquee>
+      <marquee>Developed by Team Wangan &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+        Members: Barcenilla Adrian Jay , Garcia Victor , Miones John William , Taboada Timothy , Uy Andre Lennard
+      </marquee>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
