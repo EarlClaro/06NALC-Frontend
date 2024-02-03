d4a4f4b51f2dc4f88cac4cb5c1205dfefed050bb
Andre Lennard S. Uy <andrelennard.uy@cit.edu>
2024-02-03 19:45:54 +0800

    Add Marquee tag

--- Frontend/nalc/src/App.js
+++ Frontend/nalc/src/App.js
@@ -9,15 +9,19 @@ import 'ldrs/ring'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="/admin" element={<AdminScreen />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+    <marquee>Developed by Team Wangan &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+       Members: Barcenilla Adrian Jay , Garcia Victor , Miones John William , Taboada Timothy , Uy Andre Lennard</marquee>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/admin" element={<AdminScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
